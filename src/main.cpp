2cc74b5dc14be4dc3dcd08bf536fe99e827d0c9f
Ace5tar <tace233@gmail.com>
2026-03-30 11:10:44 -0700

    fixed using string literals for allocating new char ptrs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "types/Stack.h"
+#include "types/Queue.h"
 
-int main() {
-  Stack stack;
+int main(int argc, char* argv[]) {
+  Queue queue = Queue();
+  Stack stack = Stack();
 
-  stack.push("test");
+  queue.enqueue(new const char[80]{"q1"});
+  queue.enqueue(new const char[80]{"q2"});
+  queue.enqueue(new const char[80]{"q3"});
 
+  stack.push(new const char[80]{"s1"});
+  stack.push(new const char[80]{"s2"});
+  stack.push(new const char[80]{"s3"});
+
+  std::cout << queue.peek() << std::endl;
+  queue.dequeue();
+  std::cout << queue.peek() << std::endl;
+  queue.dequeue();
+  std::cout << queue.peek() << std::endl;
+
+  std::cout << stack.peek() << std::endl;
+  stack.pop();
+  std::cout << stack.peek() << std::endl;
+  stack.pop();
   std::cout << stack.peek() << std::endl;
 }
